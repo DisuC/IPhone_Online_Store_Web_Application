1821074fe163b0e365d76f50817e1acb21598be0
Disadi Chanmini <disadichanmini@gmail.com>
2024-08-13 23:04:45 +0530

    Update firebase.js

--- product-list-app/src/firebase.js
+++ product-list-app/src/firebase.js
@@ -4,13 +4,7 @@ import {getFirestore} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA7dB6iU8oCMyq-dc1RX7Ce_mW18hZoQ20",
-    authDomain: "iphone-store-8b8d7.firebaseapp.com",
-    projectId: "iphone-store-8b8d7",
-    storageBucket: "iphone-store-8b8d7.appspot.com",
-    messagingSenderId: "580272738865",
-    appId: "1:580272738865:web:9340b3a6d895731806fb10",
-    measurementId: "G-KL3ZF87FDT"
+    //your firebase config
 };
 
 // Initialize Firebase
